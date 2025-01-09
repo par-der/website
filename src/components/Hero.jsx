1436905e5eafa393f4a378af02114f588612b66c
Kishou Arima <egorterentev58@gmail.com>
2025-01-09 22:46:14 +0400

    Hero section add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import Button from "./Button.jsx";
 import {TiLocationArrow} from "react-icons/ti";
 import {useGSAP} from "@gsap/react";
 import gsap from 'gsap';
+
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger)
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -10,7 +14,7 @@ const Hero = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedVideos, setLoadedVideos] = useState(0)
 
-    const totalVideos = 3;
+    const totalVideos = 4;
     const nextVideoRef = useRef(null);
 
     const handleVideoLoad = () => {
@@ -23,6 +27,12 @@ const Hero = () => {
         setHasClicked(true);
         setCurrentIndex(upcomingVideoIndex);
     }
+
+    useEffect(() => {
+        if(loadedVideos === totalVideos - 1) {
+            setIsLoading(false);
+        }
+    })
 
     useGSAP(() => {
         if(hasClicked) {
@@ -46,10 +56,38 @@ const Hero = () => {
         }
     }, {dependencies: [currentIndex], revertOnUpdate: true})
 
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(14% 0%, 72% 0%, 90% 90%, 0% 100%)',
+            borderRadius: '0 0 40% 10%',
+        })
+
+        gsap.from('#video-frame', {
+            clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)',
+            borderRadius: '0 0 0 10',
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '#video-frame',
+                start: 'center center',
+                end: 'bottom center',
+                scrub: true,
+            }
+        })
+    })
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
 
     return (
         <div className='relative h-dvh w-screen overflow-x-hidden'>
+            {isLoading && (
+                <div className='flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50'>
+                    <div className="three-body">
+                        <div className="three-body__dot" />
+                        <div className="three-body__dot" />
+                        <div className="three-body__dot" />
+                    </div>
+                </div>
+            )}
             <div id='video-frame' className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'>
                 <div>
                     <div
